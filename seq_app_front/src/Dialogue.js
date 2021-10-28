4a9c4f2b5c374b91e5ed13f5ed000579ea4a6f0f
Soulard Cédric <cedric.soulard.external@airbus.com>
2021-10-28 16:02:02 +0200

    Init of User Dialog App

---
+++ seq_app_front/src/Dialogue.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import Button from '@mui/material/Button';
+
+import socketIOClient from "socket.io-client";
+
+export default function Dialogue(){
+
+
+    return(
+        <div>
+            <p>Zone de comm avec user</p>
+        </div>
+    );
+}
